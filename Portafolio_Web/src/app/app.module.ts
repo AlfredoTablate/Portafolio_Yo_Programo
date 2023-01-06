72ce622cc31be9ebe118869c8a189254b8f6e178
Alfredo <109168653+AlfredoTablate@users.noreply.github.com>
2023-01-05 22:38:02 -0300

    incorporacion de acerca de y la capacitación
    hay que chequear la instalación del boostrap

--- Portafolio_Web/src/app/app.module.ts
+++ Portafolio_Web/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { LogoAPComponent } from './componets/logo-ap/logo-ap.component';
+import { SocialComponent } from './componets/social/social.component';
+import { BannerComponent } from './componets/banner/banner.component';
+import { AcercaDeComponent } from './componets/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componets/experiencia/experiencia.component';
+import { EducacionComponent } from './componets/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
