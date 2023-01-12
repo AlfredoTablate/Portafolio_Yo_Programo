27d9abd5f2132e103487d03b3b3e1adae5f2e012
Alfredo <109168653+AlfredoTablate@users.noreply.github.com>
2023-01-12 09:24:14 -0300

    corrección de bostrapp
    aunque incoorpore mejor a boostrap igual no me toma la grilla de columnas

---
+++ Portafolio_Web/backEnd/src/main/java/com/portafolioWeb/Tablate/TablateApplication.java
@@ -0,0 +1,13 @@
+package com.portafolioWeb.Tablate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablateApplication.class, args);
+	}
+
+}
---
+++ Portafolio_Web/backEnd/src/test/java/com/portafolioWeb/Tablate/TablateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolioWeb.Tablate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
