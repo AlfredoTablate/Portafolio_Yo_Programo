a15b8ffeb94dd8d7197729f757b9326843d6ab17
Alfredo <109168653+AlfredoTablate@users.noreply.github.com>
2023-01-13 11:28:42 -0300

    implementacion efectiva de boostrap

---
+++ Portafolio_Web/backEnd/src/test/java/com/PortaWeb/AAT/AatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PortaWeb.AAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
